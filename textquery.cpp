526a8e2acca8c1bf7a156e8f2213a10dd126fd0d
pkunewton <420218190@qq.com>
2018-01-24 18:16:56 +0800

    copy and move

--- textquery.cpp
+++ textquery.cpp
@@ -2,3 +2,5 @@
 // Created by Administrator on 2018/1/23 0023.
 //
 
+
+
--- textquery.h
+++ textquery.h
@@ -12,17 +12,40 @@
 #include <set>
 #include <iostream>
 #include <fstream>
+#include "Blob.h"
 
 class QueryResult;
+class Query;
+class Query_Base;
+class OrQuery;
+class AndQuery;
+class BinaryQuery;
+class NotQuery;
+class WordQuery;
+
 class TextQuery {
 public:
     using line_no = std::vector<std::string>::size_type;
-//    typedef std::vector<std::string>::size_type LineNo;
+//    typedef std::vector<std::string>::size_type line_no;
     TextQuery(std::ifstream);
     QueryResult query(const std::string &s) const ;
 private:
     std::shared_ptr<std::vector<std::string>> file;
     std::map<std::string, std::shared_ptr<set<line_no>> word_map;
 };
+
+class QueryResult {
+    friend std::ostream& print(std::ostream&, QueryResult&);
+public:
+    QueryResult();
+private:
+    std::string sought;
+    std::shared_ptr<std::set<TextQuery::line_no >> sp_lines;
+    BlobPtr<std::string> file;
+};
+
+std::ostream& print(std::ostream&, QueryResult&);
+
+
 
 #endif //PRIMER_TEXTQUERY_H
