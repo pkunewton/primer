ed08ce3962e54b5450b83db504be1cf3d56072e9
pkunewton <420218190@qq.com>
2018-02-02 12:48:50 +0800

    文字查询程序

--- textquery/notquery.cpp
+++ textquery/notquery.cpp
@@ -1,8 +1,19 @@
 //
 // Created by LiuKe on 2018/2/2.
 //
-#include "orquery.h"
+#include "notquery.h"
 
-QueryResult OrQuery::eval(const TextQuery &textQuery) const {
-
+QueryResult NotQuery::eval(const TextQuery &text) const {
+    QueryResult res = query.eval(text);
+    auto ret_lines = std::make_shared<std::set<line_no >>();
+    auto beg = res.begin(), end = res.end();
+    auto sz = res.get_file()->size();
+    for(std::size_t n = 0; n != sz; n++){
+        if(beg == end || *beg != n){
+            ret_lines->insert(n);
+        } else if(beg != end){
+            ++beg;
+        }
+    }
+    return QueryResult(rep(), ret_lines, res.get_file());
 }
--- textquery/orquery.cpp
+++ textquery/orquery.cpp
@@ -1,4 +1,11 @@
 //
 // Created by Administrator on 2018/2/2.
 //
+#include "orquery.h"
 
+QueryResult OrQuery::eval(const TextQuery &text) const {
+    auto left = lhs.eval(text), right = rhs.eval(text);
+    auto res_lines = std::make_shared<std::set<line_no >>(left.begin(), left.end());
+    res_lines->insert(right.begin(), right.end());
+    return QueryResult(rep(), res_lines, left.get_file());
+}
