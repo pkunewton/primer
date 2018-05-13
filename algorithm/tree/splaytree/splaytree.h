85b41a7f857e31fdfdbc043120cd384908043b0e
pkunewton <420218190@qq.com>
2018-05-13 21:27:48 +0800

    tree

---
+++ algorithm/tree/splaytree/splaytree.h
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2018/5/12 0012.
+//
+
+#ifndef PRIMER_SPLAYTREE_H
+#define PRIMER_SPLAYTREE_H
+typedef int ElementType;
+typedef struct SplayTreeNode *Position;
+typedef struct SplayTreeNode *SplayTree;
+
+struct SplayTreeNode;
+
+
+#endif //PRIMER_SPLAYTREE_H
