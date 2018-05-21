6890d6c0a6e2971924c33dcad0e050fccc222be8
pkunewton <420218190@qq.com>
2018-05-21 20:51:09 +0800

    avl插入实现

---
+++ algorithm/httpserver/myserver.h
@@ -0,0 +1,15 @@
+//
+// Created by Administrator on 2018/5/21.
+//
+
+#ifndef PRIMER_SERVER_H
+#define PRIMER_SERVER_H
+
+//#include <sys/socket.h>
+struct addr;
+
+struct sin_addr {
+    struct addr *myaddr;
+};
+
+#endif //PRIMER_SERVER_H
